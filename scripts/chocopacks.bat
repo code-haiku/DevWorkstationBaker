bd358e623cc955a1a9bd8f7dbd905bb6128395fc
Alexander Rosenberg <alexander.rosenberg@gmail.com>
2015-08-25 20:58:05 +0200

    Vagrant configuarion changes. Enable visual studio installation with increased chocolatey timeout.

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -2,5 +2,7 @@
 set /p PATH=<C:\Windows\Temp\PATH
 
 :: Install all the things; for example:
-cmd /c choco install -y 7zip notepadplusplus totalcommander
-REM cmd /c choco install -y --accept-license visualstudiocommunity2013 -packageParameters "--Features WebTools"
+cmd /c choco install -y 7zip
+cmd /c choco install -y notepadplusplus
+cmd /c choco install -y totalcommander
+cmd /c choco install -y --accept-license --execution-timeout=10800 visualstudiocommunity2013 -packageParameters "--Features WebTools"
